33436361a7f2c7bd321f2438cc8961e4450c8b87
ntran119 <106254807+ntran119@users.noreply.github.com>
2022-07-12 16:39:01 -0400

    Update tidytuesday_SF-Rentals.R

--- tidytuesday_2022/tidytuesday_SF-Rentals.R
+++ tidytuesday_2022/tidytuesday_SF-Rentals.R
@@ -1,10 +1,13 @@
 # tidytuesday | July 5, 2022 
 # San Francisco Rentals
-
+# @ntran119
 
 rm(list = ls()) # clear environment
-
 
 library(tidyverse)
 
 rent <-read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-07-05/rent.csv')
+
+glimpse(rent) #explore the data
+
+# compare mean price across counties, accounting for sqft
