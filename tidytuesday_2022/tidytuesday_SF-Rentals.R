27294d1a62082a6bd06986d932fdd54bfd390e0d
ntran119 <106254807+ntran119@users.noreply.github.com>
2022-07-12 16:11:21 -0400

    Create tidytuesday_SF-Rentals.R

---
+++ tidytuesday_2022/tidytuesday_SF-Rentals.R
@@ -0,0 +1,10 @@
+# tidytuesday | July 5, 2022 
+# San Francisco Rentals
+
+
+rm(list = ls()) # clear environment
+
+
+library(tidyverse)
+
+rent <-read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-07-05/rent.csv')
