3fa6fde9907d3796f09fdcb7a910b4692aa9e8ce
ntran119 <106254807+ntran119@users.noreply.github.com>
2022-07-13 12:38:41 -0400

    Update tidytuesday_SF-Rentals.R

--- tidytuesday_2022/tidytuesday_SF-Rentals.R
+++ tidytuesday_2022/tidytuesday_SF-Rentals.R
@@ -10,4 +10,27 @@ rent <-read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/m
 
 glimpse(rent) #explore the data
 
-# compare mean price across counties, accounting for sqft
+range(rent$year) # 2008-2018
+n_distinct(rent$year) # 11 years
+length(unique(rent$county)) # 11 unique counties
+colSums(is.na(rent)) # counts NAs in each column
+
+## QUESTIONS TO EXPLORE ##
+# 1. How do rent prices change over years
+# 2. Difference in rent prices from different counties
+# 3. Predict rent price based on sqft
+## QUESTIONS TO EXPLORE ##
+
+
+# 1. How do rent prices change over years
+
+q1df <- rent %>%
+  select(year, county, price) %>%
+  drop_na()
+
+q1df %>%
+  group_by(year) %>%
+  summarize(mean_price = mean(price)) %>%
+  ggplot(aes(year, mean_price)) + 
+  geom_point() +
+  geom_smooth(method = 'lm', se = FALSE)
