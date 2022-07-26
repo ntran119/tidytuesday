55ec95b3d4ea7c444ae190a0d6457c7439a52a09
ntran119 <106254807+ntran119@users.noreply.github.com>
2022-07-26 10:40:44 -0400

    Update tidytuesday_commerical-fishing.R

--- tidytuesday_2021/tidytuesday_commerical-fishing.R
+++ tidytuesday_2021/tidytuesday_commerical-fishing.R
@@ -7,3 +7,18 @@ library(tidyverse)
 
 fishing <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-06-08/fishing.csv')
 stocked <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-06-08/stocked.csv')
+
+#### Explore Data ####
+
+glimpse(fishing)
+glimpse(stocked)
+
+range(fishing$year) # 1867-2015
+range(stocked$YEAR) # 1950-2018
+
+n_distinct(fishing$year) # 149
+n_distinct(stocked$YEAR) # 67
+
+colSums(is.na(fishing))
+colSums(is.na(stocked))
+
