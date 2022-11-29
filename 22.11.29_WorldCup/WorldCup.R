4cb5403093ec6e7d248074256d7fd45a6a923caa
ntran119 <106254807+ntran119@users.noreply.github.com>
2022-11-29 14:00:50 -0500

    Update WorldCup.R

--- 22.11.29_WorldCup/WorldCup.R
+++ 22.11.29_WorldCup/WorldCup.R
@@ -10,11 +10,12 @@ library(ggplot2)
 wccup %>% ggplot(aes(x=year, y = attendance, fill = attendance)) + 
   geom_col() +
   scale_fill_gradient(low = "yellow", high = "red", na.value = NA) + 
-  labs(title = 'Attendance at WC',
+  labs(title = 'Attendance at WC (1930-2018)',
        x = 'Year',
        y = 'Attendence',
        fill = NULL) +
   scale_y_continuous(labels = scales::comma) +
+  #rotate labels 90 degrees to fit bar
   geom_text(aes(label = paste(round(attendance/1000000,2), 'M', sep=""), angle = 90, hjust = +1)) +
   theme(legend.position = "none") +
   theme(panel.grid.minor = element_blank())
