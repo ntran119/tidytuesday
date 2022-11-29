edeac0073adc231a9800805af67bb3db8f702c27
ntran119 <106254807+ntran119@users.noreply.github.com>
2022-11-29 12:00:51 -0500

    TT_22/11/29

---
+++ 22.11.29_WorldCup/WorldCup.R
@@ -0,0 +1,22 @@
+
+
+tuesdata <- tidytuesdayR::tt_load('2022-11-29')
+wcmatches <- tuesdata$wcmatches
+wccup <- tuesdata$worldcups
+
+library(dplyr)
+library(ggplot2)
+
+wccup %>% ggplot(aes(x=year, y = attendance, fill = attendance)) + 
+  geom_col() +
+  scale_fill_gradient(low = "yellow", high = "red", na.value = NA) + 
+  labs(title = 'Attendance at WC',
+       x = 'Year',
+       y = 'Attendence',
+       fill = NULL) +
+  scale_y_continuous(labels = scales::comma) +
+  geom_text(aes(label = paste(round(attendance/1000000,2), 'M', sep=""), angle = 90, hjust = +1)) +
+  theme(legend.position = "none") +
+  theme(panel.grid.minor = element_blank())
+
+            
